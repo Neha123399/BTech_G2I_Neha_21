ec09b4d135b5e892b0af59ccdb11e4257181337a
Neha123399 <nehaagarwal12340@gmail.com>
2026-06-12 12:00:13 +0530

    Add files via upload

---
+++ day 2 code 4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int num, originalNum, reverse = 0, digit;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    originalNum = num;
+
+    while (num != 0) {
+        digit = num % 10;
+        reverse = reverse * 10 + digit;
+        num = num / 10;
+    }
+
+    if (originalNum == reverse)
+        printf("%d is a Palindrome Number.\n", originalNum);
+    else
+        printf("%d is not a Palindrome Number.\n", originalNum);
+
+    return 0;
+}
